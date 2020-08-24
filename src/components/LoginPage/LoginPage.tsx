3cdd01b72cfc4a9c9674e2a5010de45237c85a06
kratyuk <kratyuk@yahoo.com>
2020-08-24 17:37:29 +0300

    loginpage

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import './LoginPage.less'
 import { Form, Button, Input } from 'antd';
 import { NavLink } from 'react-router-dom';
+import {  UserOutlined, LockOutlined  } from '@ant-design/icons';
 
 const LoginPage: React.FC = () => {
-
-  const layout = {
-    wrapperCol: { offset: 6, span: 12 },
-  };
-  const tailLayout = {
-    wrapperCol: { offset: 10, span: 16 },
-  };
 
   const onSubmit = (values: any) => {
     console.log('Success:', values);
@@ -18,8 +12,11 @@ const LoginPage: React.FC = () => {
 
   return (
     <div className="login-page">
-       <Form
-        {...layout}
+      <div className="login-page__window">
+        <img src="/logo.svg" alt="logo" />
+        <h2 className="login-page__title">Вход в личный офис</h2>
+        <Form
+        name="normal_login"
         className="login-form"
         initialValues={{
           remember: true,
@@ -27,34 +24,45 @@ const LoginPage: React.FC = () => {
         onFinish={onSubmit}
       >
         <Form.Item
-          className="login-form__input"
           name="username"
           rules={[
             {
               required: true,
-              message: 'Пожалуйста введите свой логин!',
+              message: 'Пожалуйста введите имя пользователя!',
             },
           ]}
         >
-          <Input />
+          <Input className="login-form__input" prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Имя пользователя" />
         </Form.Item>
         <Form.Item
-          className="login-form__input"
           name="password"
           rules={[
             {
               required: true,
-              message: 'Пожалуйста введите свой пароль!',
+              message: 'Пожалуйста введите пароль!',
             },
           ]}
         >
-          <Input.Password />
+          <Input
+            className="login-form__input"
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
         </Form.Item>
-        <Form.Item {...tailLayout}>
-          <Button className="login-form__button" type="primary" htmlType="submit"> Войти </Button>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form__button">
+            Войти
+          </Button>
         </Form.Item>
       </Form>
-      <NavLink className="login-page__navlink" to="/reset"> Забыли пароль? </NavLink>
+      <div className="login-form__links">
+        <NavLink className="login-form__navlink" to="/registration"> Регистрация </NavLink>
+        <NavLink className="login-form__navlink" to="/reset"> Забыли пароль? </NavLink>
+      </div>
+      <div className="login-page__circle1"/>
+      <div className="login-page__circle2"/>
+      </div>
     </div>
   );
 }
--- src/components/ResetPasswordPage/ResetPasswordPage.tsx
+++ src/components/ResetPasswordPage/ResetPasswordPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ResetPasswordPage.less'
 
 const ResetPasswordPage: React.FC = () => {
   return (
